351921d10d39286ce069d46fe4bbf80881087131
CerasCTK <ceras410@gmail.com>
2025-08-18 09:44:49 -0700

    update file setup.sh and change the file name to true context

---
+++ tools/prepare-environment.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+################################
+##### PROJECT ENVIRONMENTS #####
+################################
+TOOLS_DIR=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
+SCRIPTS_DIR="$TOOLS_DIR/scripts"
+
+export PROJECT_DIR="$(dirname $TOOLS_DIR)"
+
+FLAG_DIR="/var/lib/SwarmUAV"
+FLAG_FILE="$FLAG_DIR/prepare_done"
+
+
+################################
+########## PRE-CHECK ###########
+################################
+if [ -f $FLAG_FILE ]; then
+	echo "[INFO] This script only needs to be run once! Please run the `setup-environment.sh` script."
+	exit 0
+do
+
+################################
+### INSTALL & BUILD SCRIPTS ####
+################################
+"$SCRIPTS_DIR/install-miniconda.sh"
+"$SCRIPTS_DIR/install-ros-noetic.sh"
+"$SCRIPTS_DIR/build-mavlink-router.sh"
+"$SCRIPTS_DIR/build-px4-autopilot.sh"
+
+################################
+############# FLAG #############
+################################
+echo "[INFO] Marking prepare environment phase as done."
+if [ ! -d $FLAG_DIR ]; then
+	sudo mkdir -p $FLAG_DIR
+fi
+
+################################
+############ REBOOT ############
+################################
+# Clear stdin buffer
+while read -t 0 -n 1; do ;; done
+
+read -n 1 -s -p "[INFO] You MUST to restart your computer before jump to next step. Press any key to reboot..."
+
+echo
+echo "Rebooting now..."
+sudo reboot
